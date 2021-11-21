fa6c933a180df8408883ff112d1cf613b9987cf7
dyu <decong.yu@gmail.com>
2021-11-20 17:03:28 -0700

    add customed user module

--- back/back/settings.py
+++ back/back/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os, json
+
+with open('./param_config.json') as jsonfile:
+    config = json.load(jsonfile)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g&ka@^rzpd*x$s9w@&v6d#t&&6&x!&07f-zn(jsl_952ma8t!m'
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
